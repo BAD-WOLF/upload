8038a97fffb8501c77e790a729c243046dfd6ab5
Matheus Vieira <88906998+BAD-WOLF@users.noreply.github.com>
2021-12-25 15:13:18 -0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+        <head>
+                <meta charset="utf-8"/>
+        </head>
+
+        <body>
+                <?php
+                        if(isset($_POST["button"])){
+
+                                $uploaddir = getcwd();
+                                $uploadfile = $uploaddir . basename($_FILES['file']['name']);
+
+                                echo '<pre>';
+                                if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)){
+                                        echo "Arquivo válido e enviado com sucesso.\n";
+                                } else {
+                                        echo "Possível ataque de upload de arquivo!\n";
+                                }
+
+                                echo 'Aqui está mais informações de debug:';
+                                print_r($_FILES);
+
+                        }
+                        ?>
+                        <form enctype="multipart/form-data" method="POST">
+                                <input name="file" type="file" />
+                                <input type="submit" value="Enviar arquivo" />
+                        </form>
+
+        </body>
+
+</html>
